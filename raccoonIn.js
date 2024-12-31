cc73b6a18ba2cf4c662a5b53b84e9a5fd25296ac
ArturRaccoon <166847687+ArturRaccoon@users.noreply.github.com>
2025-01-01 00:23:04 +0100

    Update raccoonIn.js

--- raccoonIn.js
+++ raccoonIn.js
@@ -2,15 +2,12 @@ const inputField = document.getElementById('secret-input');
 const checkBtn = document.getElementById('check-btn');
 const message = document.getElementById('message');
 
-
 checkBtn.addEventListener('click', () => {
     const userInput = inputField.value.trim().toLowerCase();
 
-    if (userInput === "одного дня ми зустрінемось") {
-        // Redirect if correct
-        window.location.href = 'https://www.youtube.com/watch?v=Q3vAgdewNwM';
+    if (userInput === "одного дня ми зустрілися") {
+        window.location.href = 'newYear.html';
     } else {
-        // Show error message if incorrect
         message.textContent = "ХММ ЦІКАВО...";
         message.classList.remove('hidden');
     }
